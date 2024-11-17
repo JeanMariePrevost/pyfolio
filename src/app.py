082560ac36ebf821baa90144e2009de878fd1b7a
JeanMariePrevost <jeanmarie.prevost@outlook.com>
2024-11-17 11:11:43 -0500

    Refactor homepage to use Jinja templating, add base template, and create placeholder routes for gallery, about, contact, privacy policy, terms of service, and cookie policy

--- src/app.py
+++ src/app.py
@@ -8,7 +8,43 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html", text_block="This is a block of plain text passed from app.py.")
+    return render_template("home.jinja", text_block="This is a block of plain text passed from app.py.")
+
+
+@app.route("/gallery")
+def gallery():
+    # TODO : Currently a placeholder
+    return "This page has not yet been defined."
+
+
+@app.route("/about")
+def about():
+    # TODO : Currently a placeholder
+    return "This page has not yet been defined."
+
+
+@app.route("/contact")
+def contact():
+    # TODO : Currently a placeholder
+    return "This page has not yet been defined."
+
+
+@app.route("/privacy_policy")
+def privacy_policy():
+    # TODO : Currently a placeholder
+    return "This page has not yet been defined."
+
+
+@app.route("/terms_of_service")
+def terms_of_service():
+    # TODO : Currently a placeholder
+    return "This page has not yet been defined."
+
+
+@app.route("/cookie_policy")
+def cookie_policy():
+    # TODO : Currently a placeholder
+    return "This page has not yet been defined."
 
 
 def open_browser():
